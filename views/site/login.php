a4a6c33b3b2f75e9b4a7b885e229f8679fb87ada
RF <RF@DESKTOP-MD8R3FN>
2017-06-15 21:43:17 +0430

    login

--- views/site/login.php
+++ views/site/login.php
@@ -8,20 +8,24 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <!--<h1><?= Html::encode($this->title) ?></h1>-->
 
-    <p>Please fill out the following fields to login:</p>
+    <!--<p>Please fill out the following fields to login:</p>-->
+    <div class="panrl panel-info col-lg-4 col-md-4 col-lg-offset-4">
+        <div class="panel-heading">ورود ب بخش مدیریت</div>
+        <div class="panel-body">
+            
 
     <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
+//        'id' => 'login-form',
+//        'layout' => 'horizontal',
+//        'fieldConfig' => [
+//            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+//            'labelOptions' => ['class' => 'col-lg-1 control-label'],
+//        ],
     ]); ?>
 
         <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
@@ -29,7 +33,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $form->field($model, 'password')->passwordInput() ?>
 
         <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            'template' => "<div class=\"col-lg-offset-1 \">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ]) ?>
 
         <div class="form-group">
@@ -39,9 +43,11 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
 
     <?php ActiveForm::end(); ?>
+        </div>
+    </div>
 
-    <div class="col-lg-offset-1" style="color:#999;">
+<!--    <div class="col-lg-offset-1" style="color:#999;">
         You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
         To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
+    </div>-->
 </div>
