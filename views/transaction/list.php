d596c5c7892bcda488c008f05feb8caef7bf5712
akbarjoudi <akbar.joody@gmail.com>
2017-06-16 21:09:53 +0430

    list

--- views/transaction/list.php
+++ views/transaction/list.php
@@ -4,12 +4,15 @@ $this->title = "لیست یازی های انجام شده";
 ?>
 <div class="row">
     <div class="col-lg-12 col-md-12">
-        <div style="background:#fff">
+        <div class="panel panel-default">
+            <div class="panel-heading"><span class="glyphicon glyphicon-list"></span> لیست بازی ها انجام شده</div>
+            <div class="panel-body">
             <?=
             GridView::widget([
                 'dataProvider' => $dataProvider
             ])
             ?>
+            </div>
         </div>
     </div>
 </div>
